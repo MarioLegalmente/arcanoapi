ebc52ac96fc017d0fb3e50b3cb95580c44ce5ca5
Mario Fernandez <mefernandez@unimagdalena.edu.co>
2024-08-11 15:22:57 -0500

    delate file app.js and update code server in file index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-import http from 'http';
-import app from './app.js';
 
-const hostname = '127.0.0.1';
-const port = 5000;
+const express = require('express')
+const app = express()
+const hostname = '127.0.0.1'
+const port = 5000
+const server = app
 
-const server = http.createServer(app);
+app.get('/', (req, res) => {
+  res.send('Hello Arcanoapi!')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
+
