8f4ea6d32f966749499a44c4f83069591d430a88
Mario Fernandez <mefernandez@unimagdalena.edu.co>
2024-08-11 10:35:10 -0500

    file arcano delete and import

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 
-//import express from 'express';
+import express from 'express';
 
 
 import { v4 as uuidv4 } from 'uuid';
 
 import bodyParser from 'body-parser';
-
-import express from "./arcano.js";
 
 const app = express();
 
