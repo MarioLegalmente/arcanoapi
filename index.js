9ce8818db249aef2a623c957f681d0271b837554
Mario Fernandez <mefernandez@unimagdalena.edu.co>
2024-08-11 16:27:08 -0500

    Architecture add folders and create files name students and techers rename folder modeles for models

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const express = require('express')
 const app = express()
+const studentsRoutes = require ('./routes/studentsRoutes.js')
+
 const hostname = '127.0.0.1'
 const port = 5000
 const server = app
@@ -8,6 +10,8 @@ const server = app
 app.get('/', (req, res) => {
   res.send('Hello Arcanoapi!')
 })
+
+app.use('/students', studentsRoutes)
 
 server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
---
+++ routes/studentsRoutes.js
@@ -0,0 +1,26 @@
+const express = require ('express')
+const router = express.Router()
+
+router.get('/',(req, res) => {
+    res.json({
+        msg: 'Request students'
+    })
+})
+router.post('/',(req, res) => {
+    res.json({
+        msg: 'Create students'
+    })
+})
+router.put('/',(req, res) => {
+    res.json({
+        msg: 'Update students'
+    })
+})
+router.delete('/',(req, res) => {
+    res.json({
+        msg: 'Delete students'
+    })
+})
+
+
+module.exports = router;
